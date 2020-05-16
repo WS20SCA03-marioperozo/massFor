bcfdb571c55a1ae4c4338d9cffad1d4109246cc6
Mario <marioperozo@gmail.com>
2020-05-15 20:25:28 -0400

    Mass creation

--- massFor/ViewController.swift
+++ massFor/ViewController.swift
@@ -8,11 +8,55 @@
 
 import UIKit
 
+struct microbe {
+    let name: String;
+    let image: UIImage;
+}
+
+//if v >= 1 && v <= 16 {
+//picture.image = UIImage(named: "frame_\(v)")
+//picture.image = UIImage(named: "frame_\(v+1)");
+//v = v + 1
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let microbes: [microbe] = [
+            microbe(name: "microbe1",             image: UIImage(named: "microbe")!),
+            microbe(name: "microbe2",             image: UIImage(named: "microbe")!),
+            microbe(name: "microbe3",             image: UIImage(named: "microbe")!),
+            microbe(name: "microbe4",             image: UIImage(named: "microbe")!),
+            microbe(name: "microbe5",             image: UIImage(named: "microbe")!),
+            microbe(name: "microbe5",             image: UIImage(named: "microbe")!)
+        ];
+        
+        let stackView: UIStackView = UIStackView();
+        stackView.axis = .vertical;
+        stackView.spacing = 5.0;
+        stackView.alignment = .center;    //.leading, .center, .trailing
+        stackView.distribution = .equalCentering; //.fillEqally, .equalSpacing, .equalCentering, .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false;
+        view.addSubview(stackView);
+        
+        //Center the UIStackView in the big white view, constrain the dimensions.
+        let constraints: [NSLayoutConstraint] = [
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
+            stackView.widthAnchor .constraint(equalToConstant: 50.0),
+            stackView.heightAnchor.constraint(equalToConstant: 150.0)
+        ];
+        NSLayoutConstraint.activate(constraints);
+        
+        for microbe: microbe in microbes {
+            let picture: UIImageView = UIImageView();
+            picture.image = UIImage(named: "microbe");
+            picture.sizeToFit();
+            stackView.addArrangedSubview(picture);
+        }
     }
 
 
